62c3b9b677b458bcf40817c0532e509fa8f9b530
Gary Smith <gsmith23@ph.ed.ac.uk>
2018-10-01 12:13:58 +0100

    Update exampleOne.m

--- exampleOne.m
+++ exampleOne.m
@@ -4,12 +4,13 @@
 %
 % This code can be used to do a single reconstruction.
 %
-% path to scan folder containing raw data and a uMap
+% Path to scan folder containing raw data and a uMap
 %  Note: set to the name of a folder containing a
 %        sub-folder of raw data and a Dixon uMap sub-folder
 %        The raw data folder should contain 'Raw_Data' in 
 %        somewhere within its name.
-pathToData = 'C:\Data\JSRecon12-TestData\Ready\VE11PTestData\';
+% Reading Room PC example:
+pathToData = 'D:\Gary_RR\Ready\PETMRI_TestData_B20P_Test1\';
 
 % uMap type (Dixon)
 % NB Dixon uMap folder must contain 'DX' in its name
