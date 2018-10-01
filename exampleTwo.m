2c1470e3166bb51112c3ebeca0b3b7e19ebe206b
Gary Smith <gsmith23@ph.ed.ac.uk>
2018-10-01 12:00:03 +0100

    Update exampleTwo.m

--- exampleTwo.m
+++ exampleTwo.m
@@ -1,12 +1,17 @@
-% simple script to start reconstruction code running
+% a simple script to start the reconstruction code running
 
-% path to scan folder contating raw data and uMap/s
-%   Laptop example
+% path to scan folder containing raw data and a uMap
+%  Note: modify to the name of a folder containing a
+%        sub-folder of raw data and a Dixon uMap sub-folder
+%        The raw data folder should contain 'Raw_Data' in 
+%        somewhere within its name.
 pathToData = 'C:\Data\JSRecon12-TestData\Ready\VE11PTestData\';
 
 % uMap type (Dixon)
+% NB Dixon uMap folder must contain 'DX' in its name
 uMapType = 'DX';
 
+% path to functions
 addpath('./Functions');
 
 % execute the reconstruction process
