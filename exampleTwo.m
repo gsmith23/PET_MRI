5ff1d4d5c9319fef7f2cd9b129da6f8b9feb5fd5
Gary <gsmith23@ph.ed.ac.uk>
2018-10-01 09:18:12 +0100

    added simple one scan / one AC reconstruction example

---
+++ exampleTwo.m
@@ -0,0 +1,13 @@
+% simple script to start reconstruction code running
+
+% path to scan folder contating raw data and uMap/s
+pathToData = 'C:\Data\JSRecon12-TestData\Ready\VE11PTestData\';
+
+% uMap type (Dixon)
+uMapType = 'DX';
+
+addpath('./Functions');
+
+% execute the reconstruction process
+% with a single AC method
+doReconWithAC(uMapType, pathToData)
