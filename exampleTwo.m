20ffe7fd638261398501fcf53acce037779fb0ef
Gary Smith <gsmith23@ph.ed.ac.uk>
2018-10-01 12:08:33 +0100

    Update exampleTwo.m

--- exampleTwo.m
+++ exampleTwo.m
@@ -1,8 +1,19 @@
-% simple script to start reconstruction code running
+% Simple script to execute image reconstruction code.
+%
+%   gary.smith@ed.ac.uk  01 10 18
+%
+% This code can be used to do multiple reconstructions
+% with all available AC methods on data from multiple 
+% PET MRI scans.
+% 
+% Before running read the comments in
+% doTheReconstructions.m and follow
+% the instructions.
 
-% location of scan folder or folders
+% Location of scan folder or folders
 % NB final slash is required
-%   Reading room PC
+%   Reading room PC example below.
+%   This folder is set up and ready to use.
 pathToFolders = 'D:\Gary_RR\Ready\';
 
 % execute the reconstruction process
